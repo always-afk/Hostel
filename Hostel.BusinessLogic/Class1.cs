7f62fbbd8f8211e2af3e18f8a7cdf04ebee934d9
blvcknwh1te <vlad.erofeenko1@gmail.com>
2021-08-29 18:27:47 +0300

    Init commit

--- Hostel.BusinessLogic/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Hostel.BusinessLogic
-{
-    public class Class1
-    {
-    }
-}
--- Hostel.DataAccess/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Hostel.DataAccess
-{
-    public class Class1
-    {
-    }
-}
