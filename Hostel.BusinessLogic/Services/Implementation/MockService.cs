07c1ba4ed5b011be9e4ecd68c7a3d4067410339e
Артём Кармальков <artemkarmalkov@gmail.com>
2021-08-30 10:25:52 +0300

    Add mock

---
+++ Hostel.BusinessLogic/Services/Implementation/MockService.cs
@@ -0,0 +1,39 @@
+﻿using Hostel.DataAccess.Models.LogicModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hostel.BusinessLogic.Services.Implementation
+{
+    public class MockService : Interfaces.IMockSrvice
+    {
+        public IEnumerable<Student> GetAllStudents()
+        {
+            return new List<Student>()
+            {
+                new Student()
+                {
+                    Id = 1,
+                    FullName = "Artyom",
+                    PhoneNumber = "1234567"
+                },
+                new Student()
+                {
+                    Id = 2,
+                    FullName = "Vlad",
+                    PhoneNumber = "2345671"
+                }
+            };
+        }
+
+        public void Save(IEnumerable<Student> students)
+        {
+            foreach(var s in students)
+            {
+                Console.WriteLine($"Name:{s.FullName} - Phone Number:{s.PhoneNumber}");
+            }
+        }
+    }
+}
---
+++ Hostel.BusinessLogic/Services/Interfaces/IMockSrvice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hostel.DataAccess.Models.LogicModels;
+
+namespace Hostel.BusinessLogic.Services.Interfaces
+{
+    public interface IMockSrvice
+    {
+        IEnumerable<Student> GetAllStudents();
+        void Save(IEnumerable<Student> students);
+    }
+}
